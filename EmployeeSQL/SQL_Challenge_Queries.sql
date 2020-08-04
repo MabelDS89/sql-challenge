03c85801932853e87cfe0fe02fe9a0d7b4ef9dbd
Mabel Duran-Sanchez <65778105+MabelDS89@users.noreply.github.com>
2020-08-04 09:09:39 -0400

    Added 4 hw query.

--- EmployeeSQL/SQL_Challenge_Queries.sql
+++ EmployeeSQL/SQL_Challenge_Queries.sql
@@ -74,3 +74,17 @@ JOIN Department_Managers
 JOIN Employee_Details
 	ON Department_Managers.emp_no = Employee_Details.emp_no;
 	
+--4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+SELECT Employee_Details.emp_no, Employee_Details.last_name, Employee_Details.first_name, Departments.dept_name
+FROM Employee_Details
+JOIN Department_Employees
+	ON Employee_Details.emp_no = Department_Employees.emp_no
+JOIN Departments
+	ON Departments.dept_no = Department_Employees.dept_no;
+	
+--5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT first_name, last_name, sex
+FROM Employee_Details
+WHERE first_name == "Hercules" AND lat_name;
