e70c95657c3ab4133fa2923eca51b9fa884c3a49
Mabel Duran-Sanchez <65778105+MabelDS89@users.noreply.github.com>
2020-08-04 09:37:04 -0400

    Added 8 hw query.

--- EmployeeSQL/SQL_Challenge_Queries.sql
+++ EmployeeSQL/SQL_Challenge_Queries.sql
@@ -111,3 +111,8 @@ JOIN Departments
 WHERE dept_name = 'Sales' OR dept_name = 'Development';
 
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+SELECT Employee_Details.last_name, COUNT(last_name) AS "last_name count"
+FROM Employee_Details
+GROUP BY last_name
+ORDER BY "last_name count" DESC;
